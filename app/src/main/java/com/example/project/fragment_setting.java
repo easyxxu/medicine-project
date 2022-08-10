071bea6d940a26cde4394ece811de7fd8c0efe23
easyxxu <107910342+easyxxu@users.noreply.github.com>
2022-08-10 16:05:46 +0900

    Glide 이용해서 URL로 된 이미지 출력

--- app/src/main/java/com/example/project/fragment_setting.java
+++ app/src/main/java/com/example/project/fragment_setting.java
@@ -4,12 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
 
 public class fragment_setting extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_setting,container,false);
+        View v = inflater.inflate(R.layout.fragment_setting,container,false);
+        ImageView imageView = v.findViewById(R.id.img1);
+        String url = "https://nedrug.mfds.go.kr/pbp/cmn/itemImageDownload/152035092098000085";
+        Glide.with(getActivity()).load(url).into(imageView);
+
+        return v;
+
     }
+
 }
